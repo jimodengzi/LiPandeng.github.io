3d6310e78ef3422ef6770d513f8b37cc8938f954
lpd <850835326@qq.com>
2020-05-31 05:48:31 +0800

    first commit (lipandeng)

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
-    }
     @RequestMapping("/hi")
     public String index(){
         return "hi sb";
